c675c2cf89b51791a0f4cd3d95a1e2e13da972ab
suagrtts <kiethdaryll1@gmail.com>
2026-05-06 01:57:52 +0800

    feat: added class BattleLogStream to  redirect System.out print calls into a JTextArea widget

---
+++ src/main/java/olympicleague/gui/BattleLogStream.java
@@ -0,0 +1,37 @@
+package gui;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+public class BattleLogStream extends OutputStream {
+
+    private final JTextArea target;
+    private final StringBuilder buffer = new StringBuilder();
+
+    public BattleLogStream(JTextArea target) {
+        this.target = target;
+    }
+
+    /** Install this stream as System.out so battle log messages appear in the UI. */
+    public void install() {
+        System.setOut(new PrintStream(this, true));
+    }
+
+    @Override
+    public void write(int b) {
+        buffer.append((char) b);
+        if (b == '\n') flush();
+    }
+
+    @Override
+    public void flush() {
+        final String text = buffer.toString();
+        buffer.setLength(0);
+        SwingUtilities.invokeLater(() -> {
+            target.append(text);
+            target.setCaretPosition(target.getDocument().getLength());
+        });
+    }
+}
